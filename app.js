9221fa5739545323ebb08ca101c8b30caa27c9f6
harryRobinson <127695266+Hazbob@users.noreply.github.com>
2023-10-23 15:09:53 +0100

    potential cors fix

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ const express = require("express");
 const app = express();
 // app.use(cors())
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 
 app.use("/api", apiRouter);
 
