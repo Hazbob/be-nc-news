db1832e0dd5227302c0d0d488adbbe31cd182d60
harryRobinson <127695266+Hazbob@users.noreply.github.com>
2023-10-28 15:25:53 +0100

    potential bug fix for accepting delete requests

--- app.js
+++ app.js
@@ -24,9 +24,10 @@ const express = require("express");
 const app = express();
 // app.use(cors())
 app.use(express.json());
+app.use(cors())
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 })
